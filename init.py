6546685f5ddbe0ac61f2575b851b823734831e6b
Jasmina Pegan <jasmy_peg@hotmail.com>
2025-09-09 17:43:02 +0200

    feat: add init script and create tei script

--- init.py
+++ init.py
@@ -7,4 +7,4 @@ classla.download(lang='sl', type='standard_jos')
 # create data dir
 base_data_dir = 'data/KOST'
 if not os.path.exists(base_data_dir):
-    os.mkdir(base_data_dir)
+    os.makedirs(base_data_dir)
