1c5a6499802a4e379a0aadf80012ccf08db02591
Jasmina Pegan <jasmy_peg@hotmail.com>
2025-09-09 18:46:16 +0200

    feat: create needed dirs

--- init.py
+++ init.py
@@ -8,3 +8,6 @@ classla.download(lang='sl', type='standard_jos')
 base_data_dir = 'data/KOST'
 if not os.path.exists(base_data_dir):
     os.makedirs(base_data_dir)
+    os.makedirs('data/KOST/raw')
+    os.makedirs('data/KOST/svala')
+    os.makedirs('data/KOST/results')
